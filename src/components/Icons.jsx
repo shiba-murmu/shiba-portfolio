d3b8785d06d97be8fceee6ae50c1d38e5bad9b44
Shiba Murmu <shibamurmu001@gmail.com>
2025-07-10 23:52:16 +0530

    icons added here now .

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -2,6 +2,11 @@ import react from 'react'
 
 
 function Icons({ value }) {
+    /**
+     * 
+     * @param {In this function we are using switch case to the props to use the specific icons} val 
+     * @returns 
+     */
 
     const ChooseIcons = (val) => {
         switch (val) {
@@ -29,6 +34,16 @@ function Icons({ value }) {
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4  text-zinc-700">
                         <path fillRule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v11.69l3.22-3.22a.75.75 0 1 1 1.06 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-4.5-4.5a.75.75 0 1 1 1.06-1.06l3.22 3.22V3a.75.75 0 0 1 .75-.75Zm-9 13.5a.75.75 0 0 1 .75.75v2.25a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5V16.5a.75.75 0 0 1 1.5 0v2.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V16.5a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
                     </svg>
+                )
+            case 'codeicon':
+                // This icons is coding icons ... but not is in use.
+                return (
+                    <>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-10">
+                            <path fillRule="evenodd" d="M14.447 3.026a.75.75 0 0 1 .527.921l-4.5 16.5a.75.75 0 0 1-1.448-.394l4.5-16.5a.75.75 0 0 1 .921-.527ZM16.72 6.22a.75.75 0 0 1 1.06 0l5.25 5.25a.75.75 0 0 1 0 1.06l-5.25 5.25a.75.75 0 1 1-1.06-1.06L21.44 12l-4.72-4.72a.75.75 0 0 1 0-1.06Zm-9.44 0a.75.75 0 0 1 0 1.06L2.56 12l4.72 4.72a.75.75 0 0 1-1.06 1.06L.97 12.53a.75.75 0 0 1 0-1.06l5.25-5.25a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
+                        </svg>
+
+                    </>
                 )
         }
     }
--- src/pages/Profile/Name.jsx
+++ src/pages/Profile/Name.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import Icons from '../../components/Icons'
 function Name() {
     return (
         <>
             <div className='flex justify-center pt-3'>
-                <span className='text-4xl bold-font'>
-                    Shiba Murmu
+                <span className='text-4xl bold-font flex flex-col justify-center items-center'>
+                    {/* <Icons value='codeicon' /> */}
+                    <span>Shiba Murmu</span>
                 </span>
             </div>
         </>
