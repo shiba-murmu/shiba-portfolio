80ec0ad701d5500dc6f9e29c734e9bc8a7940044
Shiba Murmu <shibamurmu001@gmail.com>
2025-07-11 07:29:49 +0530

    skills added with icons

--- src/pages/Skills/SkillDetails.jsx
+++ src/pages/Skills/SkillDetails.jsx
@@ -1,23 +1,57 @@
 import React from 'react'
 import python_icon from '/src/assets/python.png';
+import javascript_icon from '/src/assets/javascript.png';
+import react_icon from '/src/assets/react.png';
+import Icons from '../../components/Icons';
 
-
-function Skills({value}) {
+function Skills({ value }) {
     const SkillsIcons = (val) => {
         switch (val) {
             case 'python':
                 return (
                     <>
-                        <div className='flex rounded-full border p-3 justify-evenly items-center w-60'>
+                        <div className='flex rounded-full shadow-md  p-3 justify-evenly items-center'>
                             <div>
-                                <span className='text-xl bold-font'>Python</span>
-                            </div>
-                            <div>
-                                <img src={python_icon} alt="python skills" className='w-10 h-10' />
+                                <img src={python_icon} alt="python skills" className='w-9 h-9' />
                             </div>
                         </div>
                     </>
                 )
+                
+            case 'django':
+                return (
+                    <>
+                        <div className='flex rounded-full shadow-md p-2 justify-evenly items-center'>
+                            <div>
+                                <Icons value='django' />
+                            </div>
+                        </div>
+                    </>
+                )
+
+            case 'react':
+                return (
+                    <>
+                       <div className='flex rounded-full shadow-md  p-3 justify-evenly items-center'>
+                            <div>
+                                <img src={react_icon} alt="react skills" className='w-9 h-9' />
+                            </div>
+                        </div>
+                    </>
+                )
+
+            case 'javascript':
+                return (
+                    <>
+                         <div className='flex rounded-full shadow-md  p-3 justify-evenly items-center'>
+                            <div>
+                                <img src={javascript_icon} alt="javascript skills" className='w-10 h-10' />
+                            </div>
+                        </div>
+                    </>
+                )
+            default:
+                return null
         }
     }
 
@@ -36,9 +70,14 @@ function SkillDetails() {
                     <section>
                         <span className='text-2xl introName pl-4'>Skills</span>
                     </section>
-                    <section className='flex flex-col justify-center items-center py-5 gap-5'>
+                    <section className='flex flex-wrap justify-center items-center py-5 gap-5'>
                         {/* This section is for skills... */}
                         <Skills value='python' />
+                        <Skills value='django' />
+                        <Skills value='react' />
+                        <Skills value='javascript' />
+                        <Skills value='react' />
+                        <Skills value='javascript' />
                     </section>
                 </div>
             </div>
