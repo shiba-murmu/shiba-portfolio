02f5ec516abaa784c7b8557978985c0228bde360
Shiba Murmu <shibamurmu001@gmail.com>
2025-07-11 18:02:36 +0530

    remove some spaces

--- src/contact_us/Contact_Us.jsx
+++ src/contact_us/Contact_Us.jsx
@@ -31,12 +31,11 @@ function ContactMethod({ value }) {
                                 // <div className='h-screen w-screen fixed z-50'>
                                 <div className="fixed inset-0 background-design bg-opacity-50 flex items-center justify-center z-50">
                                     <div className="bg-white p-6 rounded-lg shadow-lg w-80 text-center relative">
-                                        <div>
+                                        <div className='mb-3'>
                                             <span className='text-2xl bold-font'>Contact Us</span>
                                         </div>
                                         <form action="">
                                             <div className="mb-4">
-
                                                 <input
                                                     type="text"
                                                     id="name"
