db91dfa95a8b71ee85117a52fde8ffc0a1868bcb
Shiba Murmu <shibamurmu001@gmail.com>
2025-07-12 14:28:58 +0530

    right icons is also set for according the page.

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -28,7 +28,7 @@ function Icons({ value }) {
                 )
             case 'right':
                 return (
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-7 text-zinc-700">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-7 md:size-4 text-zinc-700">
                         <path fillRule="evenodd" d="M12.97 3.97a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06l6.22-6.22H3a.75.75 0 0 1 0-1.5h16.19l-6.22-6.22a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
                     </svg>
                 )
