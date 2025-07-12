5fda2452e872327cf65a4c4b10be345ee8ad2304
Shiba Murmu <shibamurmu001@gmail.com>
2025-07-12 23:16:38 +0530

    profile Ui fixed removed property.

--- src/pages/Profile/ProfilePhoto.jsx
+++ src/pages/Profile/ProfilePhoto.jsx
@@ -5,7 +5,7 @@ function ProfilePhoto() {
     <>
         <div className='flex justify-center items-center mt-5'>
             {/* Main container */}
-            <div className=' bg-white flex justify-center items-center'>
+            <div className=' flex justify-center items-center'>
                 {/* sub-container */}
                 {/* mask-b-from-10% mask-b-to-90%   For the mask the image */}
                 <img src={profilePicture} alt="Profile image" className='h-60 w-60 object-cover md:w-80 md:h-80 md:object-cover   rounded-full'/>
